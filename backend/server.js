81c57467c17b1db70aec7fa3ec1abdc67299cfa9
farhanhubofficial <shaciye0757@gmail.com>
2024-09-11 15:15:36 +0300

    b

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json'); // Path to db.json file
+const router = jsonServer.router('./backend/db.json'); // Update to point correctly to db.json
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3002;
 
